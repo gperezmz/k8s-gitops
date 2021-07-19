f67bdc0d2e7cc874942ec152dddf8ea33be8e0a6
Guillermo Pérez <gperezmz@users.noreply.github.com>
2021-07-19 04:03:45 +0200

    feat: install calico with tigera operator

--- scripts/install-calico.sh
+++ scripts/install-calico.sh
@@ -2,62 +2,47 @@
 
 set -exu -o pipefail
 
-export CALICO_DATASTORE_TYPE=kubernetes
-export CALICO_KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-export K8S_SVC_HOST=$(ip a | grep global | grep -v tun | awk '{print $2}' | cut -f1 -d '/')
-export K8S_SVC_PORT=6443
+export ROUTER_BGP="192.168.0.1"
+export CLUSTER_CIDR="10.42.0.0/16"
+export SERVICE_CLUSTER_CIDR="10.43.0.0/16"
+export SERVICE_EXTERNAL_CIDR="192.168.2.0/24"
+
+kubectl apply -f https://docs.projectcalico.org/archive/v3.19/manifests/tigera-operator.yaml
 
 cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: ConfigMap
+apiVersion: operator.tigera.io/v1
+kind: Installation
 metadata:
-  name: kubernetes-services-endpoint
-  namespace: kube-system
-data:
-  KUBERNETES_SERVICE_HOST: "${K8S_SVC_HOST}"
-  KUBERNETES_SERVICE_PORT: "${K8S_SVC_PORT}"
-EOF
-
-sleep 3s;
-
-kubectl apply -f https://docs.projectcalico.org/archive/v3.19/manifests/calico.yaml
-
-until [ -f /etc/cni/net.d/10-calico.conflist ]
-do
-    sleep 1;
-done
-
-cat << EOF | calicoctl apply -f -
-apiVersion: projectcalico.org/v3
-kind: IPPool
-metadata:
-  name: my.ippool-1
+  name: default
 spec:
-  blockSize: 26
-  cidr: 10.42.0.0/16
-  ipipMode: CrossSubnet
-  natOutgoing: true
-  nodeSelector: all()
+  calicoNetwork:
+    ipPools:
+    - blockSize: 26
+      cidr: ${CLUSTER_CIDR}
+      natOutgoing: Enabled
+      nodeSelector: all()
+  nodeMetricsPort: 9091
+  typhaMetricsPort: 9093
 EOF
 
-cat << EOF | calicoctl apply -f -
-apiVersion: projectcalico.org/v3
+cat <<EOF | kubectl apply -f -
+apiVersion: crd.projectcalico.org/v1
 kind: BGPPeer
 metadata:
   name: bgppeer-global-64512
 spec:
-  peerIP: 192.168.0.1
+  peerIP: ${ROUTER_BGP}
   asNumber: 64512
 EOF
 
-cat <<EOF | calicoctl apply -f -
-apiVersion: projectcalico.org/v3
+cat <<EOF | kubectl apply -f -
+apiVersion: crd.projectcalico.org/v1
 kind: BGPConfiguration
 metadata:
   name: default
 spec:
   serviceClusterIPs:
-  - cidr: 10.43.0.0/16
+  - cidr: ${SERVICE_CLUSTER_CIDR}
   serviceExternalIPs:
-  - cidr: 192.168.2.0/24
+  - cidr: ${SERVICE_EXTERNAL_CIDR}
 EOF
